621c3381e7761c5f96012106061766faccf456e0
Matthew C <m23.cullen@gmail.com>
2019-07-23 20:20:35 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,8 +14,20 @@ def third_challenge
         age: "15",
         status: "alive"
         },
-      :hero_friends => []
+      :hero_friends => [
+        {
+          name: "Benvolio",
+          age: "17",
+          attitude: "worried"
+        }
+        {
+          name: "Mercutio",
+          age: "17",
+          attitude: "hot-headed"
+        }
+        ]
    }, 
+   
    :capulet => {
       :patriarch => {
         name: "Lord Capulet",
@@ -30,7 +42,18 @@ def third_challenge
         age: "15",
         status: "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {
+          name: "Steven",
+          age: "30",
+          attitude: "confused"
+        }
+        {
+          name: "Nurse",
+          age: "44",
+          attitude: "worried"
+        }
+        ]
    }
   }
 
